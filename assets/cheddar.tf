b55ee5e3838cb117e8ad926df5388c0932d6cd01
Lucas Bergman <lucas@bergmans.us>
2023-05-01 14:35:19 +0000

    cheddar: Make a GCP service account and key
    I use GCP quite a lot. In particular each machine gets its own service
    account, and a private key for that account has to be readable somehow.
    
    Provisioning secrets on NixOS is a pain in the ass[1] that continuously
    manufactures Discourse threads, blog posts, and one-off tools. I think
    the Right Thing is probably just minting the key as part of a machine
    image, same as you would do with a SSH key/cert. Since I don't make
    images automatically yet, I chose this instead: use sops to encrypt the
    secret material - readable by the machine's SSH key - and leave the
    encrypted version in source control (and the Nix store).
    
    1. https://nixos.wiki/wiki/Comparison_of_secret_managing_schemes

--- assets/cheddar.tf
+++ assets/cheddar.tf
@@ -82,3 +82,40 @@ resource "google_dns_record_set" "bergmans_aaaa_cheddar" {
   rrdatas      = [split("/", linode_instance.cheddar.ipv6)[0]]
   ttl          = 300
 }
+
+# Create an instance-level GCP service account and make it a member of
+# IAM roles to enable DNS writes for ACME-generated certificates, log
+# shipping, and writing backups.
+
+resource "google_service_account" "instance_cheddar" {
+  account_id   = "instance-cheddar"
+  display_name = "Cheddar VM instance account"
+}
+
+resource "google_project_iam_member" "cheddar_acme_dns" {
+  project = var.gcp_project
+  role    = google_project_iam_custom_role.acme_dns.name
+  member  = "serviceAccount:${google_service_account.instance_cheddar.email}"
+}
+
+resource "google_project_iam_member" "cheddar_log_writer" {
+  project = var.gcp_project
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.instance_cheddar.email}"
+}
+
+resource "google_storage_bucket_iam_member" "cheddar_backup" {
+  bucket = google_storage_bucket.backup.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.instance_cheddar.email}"
+}
+
+resource "google_service_account_key" "instance_cheddar" {
+  service_account_id = google_service_account.instance_cheddar.id
+}
+
+resource "local_sensitive_file" "instance_cheddar" {
+  file_permission = "0600"
+  content_base64  = google_service_account_key.instance_cheddar.private_key
+  filename        = "${path.module}/cheddar-instance-private-key.json"
+}
---
+++ misc/encrypt-gcp-key.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+usage() {
+    echo "encrypt-gcp-key HOST KEYFILE" >&2
+    exit 44
+}
+
+[ -z "$1" ] && usage
+HOST="$1"; shift
+[ -z "$1" ] && usage
+KEYFILE="$1"; shift
+
+KEY="$(ssh-keyscan -t ssh-ed25519 "$HOST" 2>/dev/null | ssh-to-age)"
+exec sops --encrypt                             \
+     --age "$KEY"                               \
+     --input-type json                          \
+     --encrypted-regex '^private'               \
+     "$KEYFILE"
