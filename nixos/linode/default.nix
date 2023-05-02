e23782e01629fcd83fd5573ce7f58fea00e925b3
Lucas Bergman <lucas@bergmans.us>
2023-05-02 15:00:43 +0000

    linode: Enable systemd-networkd (and get IPv6 working)
    I read that systemd-networkd, which is something I now know exists, "should
    be preferred over networking.interfaces" because it "receives far superior
    maintenance" [1]. How exciting.
    
    1. https://nixos.wiki/wiki/Systemd-networkd

--- nixos/linode/default.nix
+++ nixos/linode/default.nix
@@ -1,9 +1,38 @@
 {...}: {
   imports = [./boot.nix];
 
+  # Use systemd-networkd for address configuration
+  # TODO: Move this to a common file; it's not Linode-specific
+  networking = {
+    useDHCP = false;
+    networkmanager.enable = false;
+  };
+  systemd.network.enable = true;
+
   networking = {
     usePredictableInterfaceNames = false;
-    useDHCP = false;
-    networkmanager.enable = true;
+    enableIPv6 = true;
+
+    # Use sysctl to disable use of RFC 4941 IPv6 temporary addresses. Linode
+    # drops outbound traffic not from the permanent address[1].
+    #
+    # 1. https://www.linode.com/community/questions/19157/ipv6-privacy-extensions
+    tempAddresses = "disabled";
+  };
+
+  systemd.network = {
+    networks."10-wan" = {
+      # The public interface on Linode is always eth0
+      matchConfig.Name = "eth0";
+
+      # Turn on DHCP for IPv4 and SLAAC for IPv6
+      networkConfig = {
+        DHCP = "ipv4";
+        IPv6AcceptRA = true;
+      };
+
+      # This link must be routable as a dependency of network-online.target
+      linkConfig.RequiredForOnline = "routable";
+    };
   };
 }
