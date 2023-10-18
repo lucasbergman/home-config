f08f2451165190c621176adf4e80504908728ac5
Lucas Bergman <lucas@bergmans.us>
2023-10-18 14:02:03 +0000

    home: Add more essentials to development shells

--- shell.nix
+++ shell.nix
@@ -4,11 +4,13 @@
   system,
 }: {
   default = pkgs.mkShell {
+    NIX_CONFIG = "extra-experimental-features = nix-command flakes";
     nativeBuildInputs = with pkgs; [
       git
       go
       home-manager
       nix
+      vim
 
       inputs.gomod2nix.packages.${system}.default
     ];
