ce112a0e0054ef09288f03c385188929de067f8c
Lucas Bergman <lucas@bergmans.us>
2023-11-21 21:32:48 +0000

    gcplogs: Ship cheddar logs to Cloud Logging

--- nixos/hosts/cheddar/default.nix
+++ nixos/hosts/cheddar/default.nix
@@ -8,6 +8,7 @@
 }: {
   imports = [
     ../../common/cloudbackup.nix
+    ../../common/gcplogs.nix
     ../../common/nginx.nix
     ../../common/global
     ../../common/users
@@ -33,6 +34,31 @@
   };
 
   slb.security.gcpInstanceKeyPath = ./gcp-instance-key.json;
+
+  slb.gcplogs = {
+    location = "linode";
+    included-units = [
+      "alertmanager.service"
+      "dovecot2.service"
+      "grafana.service"
+      # TODO: Add matrix-synapse.service, but it's super noisy
+      "murmur.service"
+      "nginx.service"
+      "nscd.service"
+      "postfix.service"
+      "prometheus-blackbox-exporter.service"
+      "prometheus-node-exporter.service"
+      "prometheus.service"
+      "sshd.service"
+      "systemd-journald.service"
+      "systemd-logind.service"
+      "systemd-networkd.service"
+      "systemd-oomd.service"
+      "systemd-resolved.service"
+      "systemd-timesyncd.service"
+      "systemd-udevd.service"
+    ];
+  };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "22.11";
