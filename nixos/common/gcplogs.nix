bcec00787765e2657499dbd93d8d5f085033f097
Lucas Bergman <lucas@bergmans.us>
2023-11-21 21:24:57 +0000

    gcplogs: A module to ship to Google Cloud Logging
    This uses Fluent Bit to send logs from the systemd journal to Cloud Logging
    (formerly Stackdriver Logging) on Google Cloud Platform. I'm a bit
    dissatisfied with this explicit list of units to ship logs from, but
    shipping everything picks up tons of junk.

---
+++ nixos/common/gcplogs.nix
@@ -0,0 +1,58 @@
+{
+  config,
+  lib,
+  mypkgs,
+  pkgs,
+  ...
+}: {
+  options.slb.gcplogs = {
+    location = lib.mkOption {
+      type = lib.types.str;
+      description = "Location string to use when shipping logs";
+      example = "us:central";
+    };
+
+    included-units = lib.mkOption {
+      type = with lib.types; listOf str;
+      description = ''
+        List of systemd units to include when shipping logs. The default
+        value, the empty list, means that all units get their logs shipped,
+        which might be unbearably noisy.
+      '';
+      default = [];
+    };
+  };
+
+  config = let
+    cfg = config.slb.gcplogs;
+  in {
+    systemd.services.fluentbit-gcplogs = let
+      # This config format is a bummer, but I can't figure out how to use
+      # YAML-format config to specify an input propery like systemd_filter
+      # more than once. (Setting it to be array-valued is a parse error.)
+      mkUnitFilters = lib.concatMapStringsSep "\n" (unit: "    systemd_filter _SYSTEMD_UNIT=${unit}");
+      configFile = pkgs.writeText "fluentbit.conf" ''
+        [INPUT]
+            Name systemd
+        ${mkUnitFilters cfg.included-units}
+
+        [OUTPUT]
+            Name stackdriver
+            Match *
+            resource generic_node
+            namespace global
+            google_service_credentials /run/gcp-instance-creds.json
+            location ${cfg.location}
+            node_id ${config.networking.hostName}.${config.networking.domain}
+      '';
+    in {
+      wantedBy = ["multi-user.target"];
+      after = ["instance-key.service" "network.target"];
+      requires = ["network.target"];
+      serviceConfig = {
+        ExecStart = "${pkgs.fluent-bit}/bin/fluent-bit --config=${configFile}";
+        Restart = "always";
+      };
+    };
+  };
+}
