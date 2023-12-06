47bc3fd617eb7f0baa3510fca952f3168c711207
Lucas Bergman <lucas@bergmans.us>
2023-12-06 14:53:18 +0000

    nix: Update to NixOS 23.11
    This also updates the state version for cheddar from 22.11 to 23.05. I'll
    read over the release notes more carefully before moving to 23.11.

--- flake.nix
+++ flake.nix
@@ -2,12 +2,12 @@
   description = "Configurations for @lucasbergman";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
