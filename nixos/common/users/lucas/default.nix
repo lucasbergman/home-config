e320f71830df4df8fd0bbca7fc689bed004f5835
Lucas Bergman <lucas@bergmans.us>
2024-01-18 15:55:10 +0000

    nixos: Add a console password for users/lucas
    Obviously, it doesn't work for remote access.

--- nixos/common/users/lucas/default.nix
+++ nixos/common/users/lucas/default.nix
@@ -2,6 +2,7 @@
   users.users.lucas = {
     isNormalUser = true;
     extraGroups = ["wheel"];
+    hashedPassword = "$6$fKBc6HqJQscMIW7h$Beoh3lUMAcHRh6uBhAyfAesGpl.ClHQlFe0Ox1VojG2tZ.Bu40sz4Hkjcm0budyFcjti5pNOUtDZi8qAUF5ZE1";
     openssh.authorizedKeys.keys = lib.pipe ./keys.json [
       builtins.readFile
       builtins.fromJSON
