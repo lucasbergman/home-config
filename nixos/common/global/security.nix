daf2b681f9d0d2c0ee976dd232977d719b5179d8
Lucas Bergman <lucas@bergmans.us>
2024-02-23 15:05:01 +0000

    security: Options to fetch secrets from GCP
    This makes an option attrset slb.security.secrets available that generates
    systemd services to fetch secrets from GCP and write them to the filesystem
    to be consumed by other services.

--- nixos/common/global/security.nix
+++ nixos/common/global/security.nix
@@ -2,6 +2,7 @@
   config,
   lib,
   pkgs,
+  mypkgs,
   ...
 }: {
   options = {
@@ -23,11 +24,68 @@
         type = lib.types.path;
         description = "Google Cloud service account encrypted private key path";
       };
+
+      secrets = lib.mkOption {
+        default = {};
+        type = with lib.types;
+          attrsOf (submodule {
+            options = {
+              outPath = lib.mkOption {
+                description = "Output path for secret material";
+                type = path;
+              };
+              template = lib.mkOption {
+                description = "Input template that is processed to substitute secret material";
+                type = path;
+              };
+              before = lib.mkOption {
+                description = "List of systemd units that should be delayed until after secrets are written";
+                type = listOf str;
+                default = [];
+              };
+              owner = lib.mkOption {
+                description = "User that will own the output file";
+                type = str;
+                default = "root";
+              };
+              group = lib.mkOption {
+                description = "Group that will own the output file; if null, output file is not group-readable";
+                type = nullOr str;
+              };
+            };
+          });
+      };
     };
   };
 
   config = let
     cfg = config.slb.security;
+    credsPath = "/run/gcp-instance-creds.json";
+    infoPath = "/run/gcp-instance-info.env";
+    mkSecretService = name: conf: let
+      mode =
+        if conf.group == null
+        then "0600"
+        else "0640";
+      group =
+        if conf.group == null
+        then "root"
+        else conf.group;
+    in {
+      description = "Fetch secret ${name}";
+      wantedBy = ["multi-user.target"];
+      inherit (conf) before;
+      after = ["instance-key.service"];
+      serviceConfig.Type = "oneshot";
+      environment.GOOGLE_APPLICATION_CREDENTIALS = credsPath;
+
+      script = ''
+        [[ -f ${conf.outPath} ]] || install -m 0600 /dev/null ${conf.outPath}
+        chown ${conf.owner}:${group} ${conf.outPath}
+        chmod ${mode} ${conf.outPath}
+        ${mypkgs.gcp-secret-subst}/bin/gcp-secret-subst ${conf.template} > ${conf.outPath}
+      '';
+    };
   in
     lib.mkIf cfg.enable {
       users.groups = {
@@ -37,40 +95,46 @@
         };
       };
 
-      systemd.services."instance-key" = {
-        description = "decrypt instance key";
-        wantedBy = ["multi-user.target"];
-        before = ["acme-${cfg.acmeHostName}.service"]; # TODO hack
-        serviceConfig = {
-          Type = "oneshot";
-          UMask = 0337;
-        };
+      systemd.services =
+        {
+          "instance-key" = {
+            description = "decrypt instance key";
+            wantedBy = ["multi-user.target"];
+            before = ["acme-${cfg.acmeHostName}.service"]; # TODO hack
+            serviceConfig = {
+              Type = "oneshot";
+              UMask = 0337;
+            };
 
-        script = with builtins; let
-          # Use the host's EdDSA 25519 key for SOPS
-          hostKeyPaths = map (getAttr "path") config.services.openssh.hostKeys;
-          edDSAKey = lib.lists.findSingle (lib.hasInfix "ed25519") "" "" hostKeyPaths;
-          sopsKeyPath = assert edDSAKey != ""; edDSAKey;
-        in ''
-          install -m 0440 -g ${config.users.groups.gcpinstance.name} \
-            /dev/null /run/gcp-instance-creds.json
-          env SOPS_AGE_KEY=$(${pkgs.ssh-to-age}/bin/ssh-to-age -private-key < "${sopsKeyPath}") \
-            ${pkgs.sops}/bin/sops --decrypt ${cfg.gcpInstanceKeyPath} > /run/gcp-instance-creds.json
+            script = with builtins; let
+              # Use the host's EdDSA 25519 key for SOPS
+              hostKeyPaths = map (getAttr "path") config.services.openssh.hostKeys;
+              edDSAKey = lib.lists.findSingle (lib.hasInfix "ed25519") "" "" hostKeyPaths;
+              sopsKeyPath = assert edDSAKey != ""; edDSAKey;
+            in ''
+              install -m 0440 -g ${config.users.groups.gcpinstance.name} /dev/null ${credsPath}
+              env SOPS_AGE_KEY=$(${pkgs.ssh-to-age}/bin/ssh-to-age -private-key < "${sopsKeyPath}") \
+                ${pkgs.sops}/bin/sops --decrypt ${cfg.gcpInstanceKeyPath} > ${credsPath}
 
-          install -m 0444 /dev/null /run/gcp-instance-info.env
-          cat >/run/gcp-instance-info.env <<EOF
-          GCE_PROJECT=$(${pkgs.jq}/bin/jq -r .project_id </run/gcp-instance-creds.json)
-          GCE_SERVICE_ACCOUNT_FILE=/run/gcp-instance-creds.json
-          EOF
-        '';
-      };
+              install -m 0444 /dev/null ${infoPath}
+              cat >${infoPath} <<EOF
+              GCE_PROJECT=$(${pkgs.jq}/bin/jq -r .project_id <${credsPath})
+              GCE_SERVICE_ACCOUNT_FILE=${credsPath}
+              EOF
+            '';
+          };
+        }
+        // lib.mapAttrs' (
+          name: value: lib.nameValuePair ("secret-" + name) (mkSecretService name value)
+        )
+        cfg.secrets;
 
       security.acme = {
         acceptTerms = true;
         defaults = {
           email = "lucas@bergmans.us";
           dnsProvider = "gcloud";
-          credentialsFile = "/run/gcp-instance-info.env";
+          credentialsFile = infoPath;
         };
 
         certs."${cfg.acmeHostName}" = {};
