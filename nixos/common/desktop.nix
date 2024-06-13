3b17f12f0f5ceccd7239269e2452b05e62a45841
Lucas Bergman <lucas@bergmans.us>
2024-06-13 17:32:53 -0500

    desktop: Switch to Plasma

--- nixos/common/desktop.nix
+++ nixos/common/desktop.nix
@@ -4,8 +4,8 @@
   ...
 }: {
   services.xserver.enable = true;
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
 
   programs.sway = {
     enable = true;
