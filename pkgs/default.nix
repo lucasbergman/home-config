851062f0e4c84e6237b5f06d9c43a5dc4b84e8a6
Lucas Bergman <lucas@bergmans.us>
2024-11-26 16:04:31 -0600

    moneydance: Turn on font antialiasing

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,6 +5,9 @@
 }:
 {
   gcp-secret-subst = pkgs.callPackage ./gcp-secret-subst { };
-  moneydance = pkgs.callPackage ./moneydance.nix { openjdk23 = pkgs-unstable.openjdk23; };
+  moneydance = pkgs.callPackage ./moneydance.nix {
+    openjdk23 = pkgs-unstable.openjdk23;
+    jvmFlags = [ "-Dawt.useSystemAAFontSettings=on" ];
+  };
   prometheus-jmx = pkgs.callPackage ./prometheus-jmx.nix { };
 }
