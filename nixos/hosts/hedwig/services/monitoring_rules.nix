831d348424e2428bdbe3c8613117cecf43167793
Lucas Bergman <lucas@bergmans.us>
2025-04-18 18:01:23 +0000

    chore: Update to the new nixfmt

--- nixos/hosts/hedwig/services/monitoring_rules.nix
+++ nixos/hosts/hedwig/services/monitoring_rules.nix
@@ -54,33 +54,39 @@ in
     }
     {
       name = "node_net";
-      rules = [
-        {
-          record = "node:receive_bytes";
-          expr = "node_network_receive_bytes_total{device=~'^(en|eth).*'}";
-        }
-        {
-          record = "node:transmit_bytes";
-          expr = "node_network_transmit_bytes_total{device=~'^(en|eth).*'}";
-        }
-      ] ++ (mkRates "node:receive_bytes") ++ (mkRates "node:transmit_bytes");
+      rules =
+        [
+          {
+            record = "node:receive_bytes";
+            expr = "node_network_receive_bytes_total{device=~'^(en|eth).*'}";
+          }
+          {
+            record = "node:transmit_bytes";
+            expr = "node_network_transmit_bytes_total{device=~'^(en|eth).*'}";
+          }
+        ]
+        ++ (mkRates "node:receive_bytes")
+        ++ (mkRates "node:transmit_bytes");
     }
     {
       name = "node_fs";
-      rules = [
-        {
-          record = "node:filesystem_avail_bytes";
-          expr = "node_filesystem_avail_bytes{job='node',fstype!~'(tmpfs|ramfs)'}";
-        }
-        {
-          record = "node:disk_read_seconds";
-          expr = "node_disk_read_time_seconds_total";
-        }
-        {
-          record = "node:disk_write_seconds";
-          expr = "node_disk_write_time_seconds_total";
-        }
-      ] ++ (mkRates "node:disk_read_seconds") ++ (mkRates "node:disk_write_seconds");
+      rules =
+        [
+          {
+            record = "node:filesystem_avail_bytes";
+            expr = "node_filesystem_avail_bytes{job='node',fstype!~'(tmpfs|ramfs)'}";
+          }
+          {
+            record = "node:disk_read_seconds";
+            expr = "node_disk_read_time_seconds_total";
+          }
+          {
+            record = "node:disk_write_seconds";
+            expr = "node_disk_write_time_seconds_total";
+          }
+        ]
+        ++ (mkRates "node:disk_read_seconds")
+        ++ (mkRates "node:disk_write_seconds");
     }
     {
       name = "node_disk";
