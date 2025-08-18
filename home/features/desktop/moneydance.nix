d45211c95ff0bc3a197d822d23471eafefe75f00
Lucas Bergman <lucas@bergmans.us>
2025-08-18 13:00:37 +0000

    cleanup: Replace Moneydance package with an override

--- home/features/desktop/moneydance.nix
+++ home/features/desktop/moneydance.nix
@@ -1,10 +1,16 @@
-{ mypkgs, ... }:
+{ pkgs, ... }:
+let
+  # Since I maintain Moneydance in nixpkgs, I mess with different versions all the time
+  pinnedMoneydance = pkgs.moneydance.overrideAttrs (prev: {
+    version = "2024.4_5253";
+    src = pkgs.fetchzip {
+      url = "https://infinitekind.com/stabledl/2024.4.5253/moneydance-linux.tar.gz";
+      hash = "sha256-wwSb3CuhuXB4I9jq+TpLPbd1k9UzqQbAaZkGKgi+nns=";
+    };
+  });
+in
 {
   home.packages = [
-    (mypkgs.moneydance.override {
-      jvmFlags = [
-        "-Dawt.useSystemAAFontSettings=on"
-      ];
-    })
+    (pinnedMoneydance.override { jvmFlags = [ "-Dawt.useSystemAAFontSettings=on" ]; })
   ];
 }
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,13 +1,8 @@
 {
   pkgs,
-  pkgs-unstable,
   ...
 }:
 {
   gcp-secret-subst = pkgs.callPackage ./gcp-secret-subst { };
-  moneydance = pkgs.callPackage ./moneydance.nix {
-    openjdk23 = pkgs-unstable.openjdk23;
-    jvmFlags = [ "-Dawt.useSystemAAFontSettings=on" ];
-  };
   prometheus-jmx = pkgs.callPackage ./prometheus-jmx.nix { };
 }
