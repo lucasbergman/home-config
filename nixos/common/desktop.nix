819bdece1fbea6d320de553703ecd05da82538c6
Lucas Bergman <lucas@bergmans.us>
2025-08-29 16:18:22 -0500

    feat(desktop): Remove desktop managers
    I have to spend some time figuring out Hyprland (etc).

--- nixos/common/desktop.nix
+++ nixos/common/desktop.nix
@@ -10,12 +10,6 @@
     pulse.enable = true;
   };
   services.pulseaudio.enable = false;
-  services.xserver = {
-    enable = true;
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
-  };
-  security.pam.services.swaylock = { };
   programs.hyprland = {
     enable = true;
     package = pkgs-unstable.hyprland;
