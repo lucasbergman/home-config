ac43242d7526777211aa59f06210307630d1bd27
Lucas Bergman <lucas@bergmans.us>
2025-08-29 14:53:51 -0500

    feat(desktop): Add a hyprland configuration
    This doesn't do much except port the default config to the Nix
    language in home-manager. It seems like the cool kids are using this
    instead of Sway these days.

--- home/features/desktop/default.nix
+++ home/features/desktop/default.nix
@@ -1,9 +1,4 @@
 { pkgs, pkgs-unstable, ... }:
-let
-  # TODO: Add some Sway stuff to the NixOS config instead of Home Manager, and
-  # if this switch is still needed, make a proper module out of it
-  enableSway = false;
-in
 {
   home.packages = [
     pkgs.dejavu_fonts
@@ -19,80 +14,17 @@ in
     pkgs-unstable.google-chrome
   ];
 
-  wayland.windowManager.sway = {
-    enable = enableSway;
-    wrapperFeatures.gtk = true;
-    extraSessionCommands = ''
-      export _JAVA_AWT_WM_NONREPARENTING=1
-    '';
-    config =
-      let
-        fonts = {
-          names = [ "DejaVu Sans" ];
-          size = 16.0;
-        };
-      in
-      {
-        inherit fonts;
-        modifier = "Mod4";
-        terminal = "alacritty";
-        startup = [ { command = "alacritty"; } ];
-        bars = [
-          {
-            inherit fonts;
-            position = "top";
-            statusCommand = "while date +'%a %F %H:%M'; do sleep 5; done";
-          }
-        ];
-      };
-  };
-
-  programs.swaylock = {
-    enable = enableSway;
-    settings = {
-      ignore-empty-password = true;
-      show-failed-attempts = true;
-      color = "111111";
-    };
-  };
-
-  services.mako.enable = enableSway;
-
-  services.swayidle = {
-    enable = enableSway;
-    timeouts = [
-      {
-        timeout = 180;
-        command = "${pkgs.swaylock}/bin/swaylock --daemonize";
-      }
-    ];
-  };
-
-  xdg.portal = {
-    enable = enableSway;
-
-    # TODO: This is needlessly blunt
-    #
-    # I wonder if we can get away with just using the GTK backend for everything
-    # except (say) GNOME for screencasting with Google Chrome, etc.
-    config.common.default = [
-      "gnome"
-      "gtk"
-      "wlr"
-    ];
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gnome
-      xdg-desktop-portal-gtk
-      xdg-desktop-portal-wlr
-    ];
-  };
-
-  programs.alacritty = {
+  wayland.windowManager.hyprland = {
     enable = true;
-    settings = {
-      font.size = 18;
-    };
+    settings = import ./hyprland.nix;
+
+    # Installed at the NixOS level
+    package = null;
+    portalPackage = null;
   };
+
+  programs.kitty.enable = true;
+  programs.wofi.enable = true;
 
   programs.vscode = {
     enable = true;
