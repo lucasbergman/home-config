735204bf60d01e6acac12afa5d9b9ed6bb66d182
Lucas Bergman <lucas@bergmans.us>
2026-08-06 16:20:35 -0500

    mail: Fix notmuch setup for Fastmail
    This tells mbsync to keep downloading from the IMAP server frequently but
    adds a setup that backs up from Fastmail daily, just in case they have data
    loss at some point. Notmuch only indexes the former.

--- home/modules/notmuch/default.nix
+++ home/modules/notmuch/default.nix
@@ -24,20 +24,46 @@
     in
     {
       accounts.email.maildirBasePath = "/home/lucas/mail";
-      accounts.email.accounts.${slb.notmuch.primaryMailAccount} = {
-        maildir.path = "spool";
-        imap.host = "pop.bergmans.us";
-        smtp = {
-          host = "smtp.bergmans.us";
-          port = 587;
-          tls.useStartTls = true;
+
+      # Handle the mail spool sitting on the IMAP server; real mail gets sent to
+      # Fastmail, so this is going to be stuff for postmaster@ etc.
+      accounts.email.accounts = {
+        ${slb.notmuch.primaryMailAccount} = {
+          primary = true;
+          maildir.path = "spool";
+          imap.host = "pop.bergmans.us";
+          smtp = {
+            host = "smtp.bergmans.us";
+            port = 587;
+            tls.useStartTls = true;
+          };
+          userName = "lucas@bergmans.us";
+          passwordCommand = "cat ~/.secret/mail";
+          mbsync = {
+            enable = true;
+            create = "both";
+            expunge = "both";
+          };
         };
-        userName = "lucas@bergmans.us";
-        passwordCommand = "cat ~/.secret/mail";
-        mbsync = {
-          enable = true;
-          create = "maildir";
-          expunge = "imap";
+
+        # Do a one-way sync from Fastmail to disk, just in case they have data loss
+        fastmail = {
+          maildir.path = "/home/lucas/mail-backup/fastmail";
+          imap = {
+            host = "imap.fastmail.com";
+            port = 993;
+            tls.enable = true;
+          };
+          userName = "lucasbergman@fastmail.com";
+          passwordCommand = "cat ~/.secret/fastmail-app-password";
+          mbsync = {
+            enable = true;
+            create = "maildir";
+            expunge = "none";
+            extraConfig.channel = {
+              Sync = "Pull";
+            };
+          };
         };
       };
 
@@ -58,6 +84,26 @@
         enable = true;
         frequency = "*:0/10";
         postExec = "${pkgs.notmuch}/bin/notmuch new && ${pkgs.afew}/bin/afew --tag --new";
+      };
+
+      # Hack the mbsync service to just sync the spool account, not everything
+      systemd.user.services.mbsync.Service.ExecStart =
+        lib.mkForce "${pkgs.isync}/bin/mbsync ${slb.notmuch.primaryMailAccount}";
+
+      # Separate mbsync setup that backs up fastmail daily
+      systemd.user.services.mbsync-fastmail = {
+        Unit.Description = "mbsync fastmail backup service";
+        Service = {
+          ExecStart = "${pkgs.isync}/bin/mbsync fastmail";
+        };
+      };
+      systemd.user.timers.mbsync-fastmail = {
+        Unit.Description = "Daily mbsync fastmail backup timer";
+        Timer = {
+          OnCalendar = "daily";
+          Persistent = true;
+        };
+        Install.WantedBy = [ "timers.target" ];
       };
     }
   );
